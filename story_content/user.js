b1934fb6576d7d0d09b01ac9fd69b8c5faf7bce5
hafni-carefully <hafni863@guru.sma.belajar.id>
2024-09-26 14:55:32 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,19 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5iFYorvdRJl":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio – document.getElementByid(‘bgsong’);
+audio.src =”audiosong1.mp3”;
+audio.load();
+audio.play();
+
+}
+
